435f852d7606dc3c941eeada1ff956588e3486a3
shahar-stack <32692649+shahar-stack@users.noreply.github.com>
2025-03-30 18:31:59 +0300

    Add playGame function Added a function which calls playRound 5 times, thus playing 5 rounds. Declared the playRound function and score variables inside it. Console outputs appropriate messages depending on the scores at the end.

--- myscripts.js
+++ myscripts.js
@@ -37,55 +37,75 @@ function capitalizeFirst (string) {
     return first+string;
 }
 
-function playRound (humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    humanChoice = capitalizeFirst(humanChoice);
-    // Set it to be case-insensitive, human choice will always equal "Rock"/"Paper"/"Scissors"
-    // with only the first letter capitalized.
 
-    switch (humanChoice) {
-        case "Rock":
-            if (computerChoice == "Paper") {
-               computerScore++;
-               console.log("You lose! Paper beats Rock."); 
-            } else if (computerChoice == "Scissors") {
-                humanScore++;
-                console.log("You win! Rock beats Scissors."); 
-            } else {
-                console.log(`Tie! You both chose ${humanChoice}`);
-            }
-            break;
-        
-        case "Paper":
+// For the playGame function we will run a simple for loop for 5 iterations.
+// Within each iteration we will call the playRound() function and increase the appropriate score accordingly.
+
+function playGame () {
+
+    function playRound (humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+        humanChoice = capitalizeFirst(humanChoice);
+        // Set it to be case-insensitive, human choice will always equal "Rock"/"Paper"/"Scissors"
+        // with only the first letter capitalized.
+    
+        switch (humanChoice) {
+            case "Rock":
+                if (computerChoice == "Paper") {
+                   computerScore++;
+                   console.log("You lose! Paper beats Rock."); 
+                } else if (computerChoice == "Scissors") {
+                    humanScore++;
+                    console.log("You win! Rock beats Scissors."); 
+                } else {
+                    console.log(`Tie! You both chose ${humanChoice}`);
+                }
+                break;
+            
+            case "Paper":
+                if (computerChoice == "Rock") {
+                    humanScore++;
+                    console.log("You win! Paper beats Rock."); 
+                 } else if (computerChoice == "Scissors") {
+                     computerScore++;
+                     console.log("You lose! Scissors beats Paper."); 
+                 } else {
+                    console.log(`Tie! You both chose ${humanChoice}`);
+                }
+                 break;
+            
+            case "Scissors":
             if (computerChoice == "Rock") {
-                humanScore++;
-                console.log("You win! Paper beats Rock."); 
-             } else if (computerChoice == "Scissors") {
-                 computerScore++;
-                 console.log("You lose! Scissors beats Paper."); 
-             } else {
-                console.log(`Tie! You both chose ${humanChoice}`);
-            }
-             break;
-        
-        case "Scissors":
-        if (computerChoice == "Rock") {
-            humanScore++;
-            console.log("You lose! Rock beats Scissors."); 
-            } else if (computerChoice == "Paper") {
                 computerScore++;
-                console.log("You win! Scissors beats Paper."); 
-            } else {
-                console.log(`Tie! You both chose ${humanChoice}`);
-            }
-            break;
+                console.log("You lose! Rock beats Scissors."); 
+                } else if (computerChoice == "Paper") {
+                    humanScore++;
+                    console.log("You win! Scissors beats Paper."); 
+                } else {
+                    console.log(`Tie! You both chose ${humanChoice}`);
+                }
+                break;
+        }
+    
+    }
+    
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
     }
 
+
+    if (humanScore == computerScore) {
+        console.log(`Tie! You tied the computer ${humanScore} to ${computerScore}!`);
+    } else if (humanScore > computerScore) {
+        console.log(`Congrats user! You beat the computer ${humanScore} to ${computerScore}!`);
+    } else {
+        console.log(`Unfortunately, the computer emerged victorious. You lost ${humanScore} to ${computerScore}!`);
+    }
 }
 
-let humanScore = 0;
-let computerScore = 0;
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
